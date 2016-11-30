7437f7a677a4db3e2fca9b8ceb77539e162d6baf
François Côté <francois@francoiscote.net>
2016-11-30 00:08:03 -0500

    2015-04, part1

---
+++ 04.js
@@ -0,0 +1,30 @@
+let crypto = require('crypto');
+
+
+class Hasher {
+	constructor(key) {
+		this.key = key
+		this.current = 0;
+		this.match = false;
+	}
+	
+	findMatch() {	
+		while (!this.match) {
+			const md5sum = crypto.createHash('md5');
+			md5sum.update(this.key + '' + this.current);
+			const hexHash = md5sum.digest('hex');
+			if (hexHash.substr(0, 6) === '00000') {
+				this.match = this.current;
+			} else {
+				this.current++;
+			}
+		}
+		
+		return this.match;
+	}
+		
+}
+
+let h = new Hasher('ckczppom');
+const v = h.findMatch();
+console.log(v);
