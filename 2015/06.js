62ea493b3d7bd0c1992b5213ce7e3a78e79091e3
François Côté <francois@francoiscote.net>
2017-07-18 07:50:55 -0400

    Day 06, refactor, still buggy.

--- 2015/06.js
+++ 2015/06.js
@@ -1,90 +1,94 @@
 const input = require('./inputs/06').split('\n');
 
 class Grid {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        const row = Array(width).fill(false);
-        this.grid = Array(width).fill(row);
-    }
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    const row = Array(width).fill(false);
+    this.grid = Array(width).fill(row);
+  }
 
-    applyOnGrid(xFrom, yFrom, xTo, yTo, operation) {
-        if (typeof operation !== "function") {
-            throw "Operation must be a function";
+  applyOnGrid(xFrom, yFrom, xTo, yTo, operation) {
+    if (typeof operation !== "function") {
+      throw "Operation must be a function";
+    }
+    for (var h = yFrom; h <= yTo; h++) {
+      for (var w = xFrom; w <= xTo; w++) {
+        operation.call(this, h, w);
+      }
+    }
+  }
+
+  do(command, xFrom, yFrom, xTo, yTo) {
+    console.log(command, xFrom, yFrom, xTo, yTo);
+
+    let actionFunc = null;
+
+    switch (command) {
+      case "toggle":
+        actionFunc = (h, w) => {
+          this.grid[h][w] = !this.grid[h][w]
         }
-        for (var h = yFrom; h <= yTo; h++) {
-            for (var w = xFrom; w <= xTo; w++) {
-                operation.call(this, h, w);
-            }
+        break;
+      case "turnon":
+        actionFunc = (h, w) => {
+          this.grid[h][w] = true;
         }
-    }
-    setLight(h, w, lightValue) {
-        this.grid[h][w] = lightValue;
-    }
-    getLight(h, w) {
-        return this.grid[h][w];
-    }
-    getLightsOnCount() {
-        // Two-dimensional count
-        return this.grid.reduce((memo, row) => {
-            const rowCount = row.reduce((memo, light) => {
-                return memo += (light)
-                    ? 1
-                    : 0;
-            }, 0);
-            return memo += rowCount;
-        }, 0);
-    }
-
-    toggle(xFrom, yFrom, xTo, yTo) {
-        console.log('toggle', xFrom, yFrom, xTo, yTo);
-        this.applyOnGrid(xFrom, yFrom, xTo, yTo, (h, w) => {
-            this.setLight(h, w, !this.grid[h][w]);
-        });
-    }
-    turnon(xFrom, yFrom, xTo, yTo) {
-        console.log('turnon', xFrom, yFrom, xTo, yTo);
-        this.applyOnGrid(xFrom, yFrom, xTo, yTo, (h, w) => {
-            this.setLight(h, w, true);
-        });
-    }
-    turnoff(xFrom, yFrom, xTo, yTo) {
-        console.log('turnoff', xFrom, yFrom, xTo, yTo);
-        this.applyOnGrid(xFrom, yFrom, xTo, yTo, (h, w) => {
-            this.setLight(h, w, false);
-        });
-    }
-    do 
-        (command, xFrom, yFrom, xTo, yTo) {
-            if(!typeof this[command] === 'function ') {
-                throw "command must be one of throw, turnon or turnoff";
-            }
-            this[command].call(this, xFrom, yFrom, xTo, yTo);
+        break;
+      case "turnoff":
+        actionFunc = (h, w) => {
+          this.grid[h][w] = false;
         }
+        break;
+      default:
+        console.log("Nothing to do");
     }
 
-    class InstructionsParser {
-        constructor(inst, grid) {
-            this.inst = inst;
-            this.grid = grid;
-        }
-        parse() {
-            this.inst.map((i, d) => {
-                const match = i.match(/(toggle|turn (on|off))\s(\d{1,3})\,(\d{1,3})\sthrough\s(\d{1,3})\,(\d{1,3})/i);
-                // Format Values
-                const command = match[1].split(' ').join('');
-                const xFrom = parseInt(match[3], 10);
-                const yFrom = parseInt(match[4], 10);
-                const xTo = parseInt(match[5], 10);
-                const yTo = parseInt(match[6], 10);
+    this.applyOnGrid(xFrom, yFrom, xTo, yTo, actionFunc);
+  }
 
-                this.grid.do 
-                    (command, xFrom, yFrom, xTo, yTo);
-                });}
-        }
 
-        const part1 = new Grid(1000, 1000);
+  // ------------
+  // READ OUT METHODS
+  // ------------
+  getLight(h, w) {
+    return this.grid[h][w];
+  }
+  countLightsOn() {
+    // Two-dimensional count
+    return this.grid.reduce((memoRow, row) => {
+      const debugRow = [];
+      const rowCount = row.reduce((memoLight, light) => {
+        return memoLight += (light) ? 1 : 0;
+      }, 0);
+      return memoRow += rowCount;
+    }, 0);
+  }
+}
 
-        const parser1 = new InstructionsParser(input, part1);
-        parser1.parse();
-        console.log(part1.getLightsOnCount());
+class InstructionsParser {
+  constructor(inst, grid) {
+    this.inst = inst;
+    this.grid = grid;
+  }
+  parse() {
+    this.inst.map((i, d) => {
+      const match = i.match(/(toggle|turn (on|off))\s(\d{1,3})\,(\d{1,3})\sthrough\s(\d{1,3})\,(\d{1,3})/i);
+
+      // Format Values
+      const command = match[1].split(' ').join('');
+      const xFrom = parseInt(match[3], 10);
+      const yFrom = parseInt(match[4], 10);
+      const xTo = parseInt(match[5], 10);
+      const yTo = parseInt(match[6], 10);
+
+      this.grid.do(command, xFrom, yFrom, xTo, yTo);
+    });
+  }
+}
+
+const part1 = new Grid(1000, 1000);
+
+const parser1 = new InstructionsParser(input, part1);
+parser1.parse();
+console.log(part1.countLightsOn());
