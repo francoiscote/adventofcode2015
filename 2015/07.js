29d3f10a31dac62efdb82104191827eb8a72df2f
François Côté <francois@francoiscote.net>
2017-07-18 08:47:29 -0400

    progress on 07.js

---
+++ 2015/07.js
@@ -0,0 +1,36 @@
+const input07 = require('./inputs/07').split('\n');
+
+
+class InstructionsParser {
+  constructor(input) {
+    this.input = input;
+  }
+
+  parse() {
+    return this.input.map((instr) => {
+      let instruction = {};
+
+      let splited = instr.split(' -> ');
+
+      instruction[splited[1]] = { value: splited[0], answer: null, resolved: 0};
+
+      return instruction;
+    });
+  }
+}
+
+const ip = new InstructionsParser( input07 );
+console.log( ip.parse() );
+
+// {
+//   'ad': {
+//     type: "operator"
+//     value: "er OR ai"
+//     answer: 123
+//     resolved: 1
+//   }
+// }
+//
+// 'lf RSHIFT 1 -> ly'
+// { 'ly':
+//   value: 'lf RSHIFT 1'}
