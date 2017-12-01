463aedc02244ef02895687df55634700412d884a
François Côté <francois@francoiscote.net>
2017-12-01 17:09:07 -0500

    2017/01, part 2

--- 2017/01/index.js
+++ 2017/01/index.js
@@ -3,11 +3,20 @@ const { splitInt } = require('../../utils/strings');
 
 const numbers = splitInt(input);
 
-let solution = 0;
+let solution1 = 0;
+const getCompareIndexPart1 = (index, array) => {
+  return (index === array.length - 1) ? 0 : index + 1;
+}
+
+let solution2 = 0;
+const getCompareIndexPart2 = (index, array) => {
+  return (index + (array.length / 2)) % array.length;
+}
 
 numbers.forEach((value, index) => {
-  const compareToIndex = (index === numbers.length-1) ? 0 : index+1;
-  solution += (value === numbers[compareToIndex]) ? value : 0;
+  solution1 += (value === numbers[getCompareIndexPart1(index, numbers)]) ? value : 0;
+  solution2 += (value === numbers[getCompareIndexPart2(index, numbers)]) ? value : 0;
 });
 
-console.log('Part 1:', solution);
+console.log('Part 1:', solution1);
+console.log('Part 2:', solution2);
