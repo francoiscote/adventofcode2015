c7cee028e11e87ab3cab137088b5241d3e2c9b70
François Côté <francois@francoiscote.net>
2017-12-06 10:58:50 -0500

    2017/06

---
+++ 2017/06/index.js
@@ -0,0 +1,78 @@
+const input = require('./input');
+const _ = require('lodash');
+
+// -------------------------------------
+// PART 1
+// -------------------------------------
+let solution1 = null;
+
+class MemoryManager {
+  constructor(blocks) {
+    this.blocks = blocks.split('\t').map( m => parseInt(m) );
+    this.states = [];
+    this.duplicatedIndex = null;
+  }
+
+  repair() {
+    let duplicatedState = false;
+
+    while (!duplicatedState) {
+      const stateHash = this.currentStateHash;
+      const duplicatedIndex = this.states.indexOf(stateHash);
+
+      if (duplicatedIndex > -1) {
+        duplicatedState = true;
+        this.duplicatedIndex = duplicatedIndex;
+      } else {
+        this.saveState(stateHash);
+        this.redistribute(this.firstBiggestBlockIndex);
+      }
+    }
+    console.log(`Memory Repair done.`);
+  }
+
+  redistribute(fromIndex) {
+    const blocks = this.blocks[fromIndex];
+    this.blocks[fromIndex] = 0;
+
+    for (let i = 1; i <= blocks; i += 1) {
+      // loop around
+      const nextIndex = (fromIndex + i) % this.blocks.length;
+
+      this.blocks[nextIndex] += 1;
+    }
+  }
+
+  saveState(state) {
+    this.states.push(state);
+  }
+
+  get currentStateHash() {
+    return this.blocks.join('-');
+  }
+
+  get firstBiggestBlockIndex() {
+    const bigValue = Math.max(...this.blocks);
+    return this.blocks.findIndex( (b) => b === bigValue );
+  }
+
+  get redisAmount() {
+    return this.states.length;
+  }
+
+  get loopSize() {
+    return this.redisAmount - this.duplicatedIndex;
+  }
+}
+
+const mem = new MemoryManager(input);
+mem.repair();
+
+console.log('Part 1:', mem.redisAmount);
+
+// -------------------------------------
+// PART 2
+// -------------------------------------
+let solution2 = null;
+
+console.log('Part 2:', mem.loopSize);
