63bc25a5e67c7da7028cde08105bd5febed45c7b
François Côté <francois@francoiscote.net>
2017-12-06 09:24:35 -0500

    template fix

--- _template/index.js
+++ _template/index.js
@@ -4,13 +4,13 @@ const _ = require('lodash');
 // -------------------------------------
 // PART 1
 // -------------------------------------
-const solution1 = null;
+let solution1 = null;
 
 console.log('Part 1:', solution1);
 
 // -------------------------------------
 // PART 2
 // -------------------------------------
-const solution2 = null;
+let solution2 = null;
 
-console.log('Part 1:', solution2);
+console.log('Part 2:', solution2);
