65206e1635447a5ca6373772a5f46b63cd66b2ce
François Côté <francois@francoiscote.net>
2017-12-08 08:30:23 -0500

    uncompleted 2017/07

---
+++ 2017/07/index.js
@@ -0,0 +1,30 @@
+const input = require('./input');
+const _ = require('lodash');
+
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+// -------------------------------------
+// PART 1
+// -------------------------------------
+let solution1 = null;
+
+console.log('Part 1:', solution1);
+
+// -------------------------------------
+// PART 2
+// -------------------------------------
+let solution2 = null;
+
+console.log('Part 2:', solution2);
