15d7c15ee51b1be50e00d69c0795fec228319117
François Côté <francois@francoiscote.net>
2019-12-02 21:53:17 -0500

    utils/string cleanup

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,25 +1,7 @@
 /* eslint import/prefer-default-export: "off" */
 
-export const split = (string = '', delimiter = '', transform = v => v) =>
-  string.split(delimiter).map(v => transform(v))
+export const split = (string = '', delimiter = ',', transform = v => v) =>
+  string.split(delimiter).map(transform)
 
 export const splitLines = (string = '', transform = undefined) =>
   split(string, /\r?\n/, transform)
-
-/**
- * This utility function will get a string of numbers
- * and ouput an array of integers from those numbers.
- * @param {string} string original input string
- */
-
-export const splitInt = string => {
-  string.split('').map(v => {
-    const intVal = parseInt(v, 10)
-
-    if (typeof intVal !== 'number') {
-      throw new TypeError(`Character ${v} is not a number`)
-    }
-
-    return intVal
-  })
-}
